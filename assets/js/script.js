4be556b63e503d30497b7b40584755560c77d462
Kevin <kevinoc554@gmail.com>
2021-05-06 16:15:19 +0000

    feat: add js to manually show/hide instructions div

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
 // Offcanvas effect on navbar adapted from Bootstrap
 $('.navbar-toggler').click(function () {
-    $('#navbarNav').toggleClass('open')
-})
+    $('#navbarNav').toggleClass('open');
+});
+
+// Manual toggle to show/hide Instructions text
+// Toggles classes to change FA chevron
+$('#instructionsToggle').click(function () {
+    $(this).toggleClass('fa-chevron-down');
+    $(this).toggleClass('fa-chevron-up');
+    $('#instructionsText').slideToggle();
+});
