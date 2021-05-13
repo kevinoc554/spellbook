40e1c617d407c5a06ff7dd3525eb7089b38bcaed
Kevin <kevinoc554@gmail.com>
2021-05-13 16:19:10 +0000

    feat: add styling and js to fade in/out return to top button, tidied js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,49 @@
-// Trigger DataTables onload and sort by Level
-$(document).ready( function () {
+$(document).ready(function () {
+    // Trigger DataTables onload and sort by Level
     $('#spell-list-table').DataTable({
-        "order": [ 1, 'asc' ],
+        "order": [1, 'asc'],
         "info": false,
         "paging": false
     });
-} );
 
-// Offcanvas effect on navbar adapted from Bootstrap
-$('.navbar-toggler').click(function () {
-    $('#navbarNav').toggleClass('open');
-});
+    // Offcanvas effect on navbar adapted from Bootstrap
+    $('.navbar-toggler').click(function () {
+        $('#navbarNav').toggleClass('open');
+    });
 
-// Manual toggle to show/hide Instructions text
-// Toggles classes to change FA chevron
-$('#instructionsToggle').click(function () {
-    $(this).toggleClass('fa-chevron-down');
-    $(this).toggleClass('fa-chevron-up');
-    $('#instructionsText').slideToggle();
-});
+    // Manual toggle to show/hide Instructions text
+    // Toggles classes to change FA chevron
+    $('#instructionsToggle').click(function () {
+        $(this).toggleClass('fa-chevron-down');
+        $(this).toggleClass('fa-chevron-up');
+        $('#instructionsText').slideToggle();
+    });
 
-// Manual toggle to show/hide Materials text
-$('#materialsListToggle').click(function () {
-    $(this).toggleClass('fa-chevron-down');
-    $(this).toggleClass('fa-chevron-up');
-    $('#materialsText').slideToggle('fast');
-});
+    // Manual toggle to show/hide Materials text
+    $('#materialsListToggle').click(function () {
+        $(this).toggleClass('fa-chevron-down');
+        $(this).toggleClass('fa-chevron-up');
+        $('#materialsText').slideToggle('fast');
+    });
 
-// Manual toggle to show/hide Damages at higher levels text
-$('#damageListToggle').click(function () {
-    $(this).toggleClass('fa-chevron-down');
-    $(this).toggleClass('fa-chevron-up');
-    $('#damagesText').slideToggle();
+    // Manual toggle to show/hide Damages at higher levels text
+    $('#damageListToggle').click(function () {
+        $(this).toggleClass('fa-chevron-down');
+        $(this).toggleClass('fa-chevron-up');
+        $('#damagesText').slideToggle();
+    });
+
+    // Fade Return to Top button in and out
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('#toTopBtn').fadeIn();
+        } else {
+            $('#toTopBtn').fadeOut();
+        }
+    })
+
+    // Smooth scroll to top when btn is clicked 
+    $('#toTopBtn').click(function () {
+        console.log($(this));
+    });
 });
